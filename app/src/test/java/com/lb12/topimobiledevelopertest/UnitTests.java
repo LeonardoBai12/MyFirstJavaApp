3556878a5e365784f67ae6b9f3147b9fc8b72507
LeonardoBai12 <69223422+LeonardoBai12@users.noreply.github.com>
2020-12-27 14:52:26 -0300

    Updated some dependencies and added two unit tests

---
+++ app/src/test/java/com/lb12/topimobiledevelopertest/UnitTests.java
@@ -0,0 +1,41 @@
+package com.lb12.topimobiledevelopertest;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
+import static com.lb12.topimobiledevelopertest.di.util.Utils.extractYoutubeId;
+import static com.lb12.topimobiledevelopertest.ui.ingredients.IngredientsModel.addIfNotEmptyOrNull;
+
+public class UnitTests extends TestCase {
+
+    @Test
+    public void testExtractYoutubeId() throws MalformedURLException {
+
+        String url = "https://www.youtube.com/watch?v=bzJDimvPW1Y";
+
+        String youtubeID = extractYoutubeId( url );
+        assertEquals("bzJDimvPW1Y", youtubeID );
+
+    }
+
+    @Test
+    public void testaddIfNotEmptyOrNull(){
+
+        ArrayList<String> ingredientsList = new ArrayList<String>();
+
+        addIfNotEmptyOrNull(ingredientsList, "ingredient1");
+        addIfNotEmptyOrNull(ingredientsList, null);
+        addIfNotEmptyOrNull(ingredientsList, "ingredient2");
+        addIfNotEmptyOrNull(ingredientsList, "");
+        addIfNotEmptyOrNull(ingredientsList, "     ");
+        addIfNotEmptyOrNull(ingredientsList, "ingredient3");
+
+        assertEquals(3,ingredientsList.size());
+    }
+
+
+
+}
